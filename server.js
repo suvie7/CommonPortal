39385329ca3b09e6799b4c0768edbd39e8efa51d
suvie7 <suvie7bhattt@gmail.com>
2020-04-18 20:15:43 +0530

    commit

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join('dad-prjct/src/index.html'));
+res.sendFile(path.join(__dirname, 'src/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
